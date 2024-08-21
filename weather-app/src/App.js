1b20efb1483795c9627ea210fc6753ea799c3c65
claudia.negrea <claudia.negrea11@gmail.com>
2024-08-21 09:58:01 +0300

    added weather-app

---
+++ weather-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from './components/weather';
+import Search from './components/search';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
