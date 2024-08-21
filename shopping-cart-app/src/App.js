37de1e09e69c6067700eb22c3e2f5636a9155509
claudia.negrea <claudia.negrea11@gmail.com>
2024-08-21 16:55:12 +0300

    added shopping-cart-app

---
+++ shopping-cart-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import Cart from './pages/cart';
+import Header from './components/header';
+
+function App() {
+  return (
+    <div >
+      <Header/>
+      <Routes>
+        <Route extract path='/' element={<Home />}>
+
+        </Route>
+        <Route  path='/cart' element={<Cart />}>
+
+        </Route>
+      </Routes>
+
+
+    </div>
+  );
+}
+
+export default App;
